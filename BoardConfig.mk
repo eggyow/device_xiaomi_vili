80a4efee175d72e6b9dedf48dfcd9121a8f3c22c
blazerpaul15 <blazerpaul15@gmail.com>
2022-10-12 04:12:32 +0530

    vili: Configure boot kernel modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,15 @@ TARGET_BOOTLOADER_BOARD_NAME := vili
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/vili_QGKI.config
 
+# Kernel modules
+BOOT_KERNEL_MODULES := \
+    fts_touch_spi.ko \
+    hwid.ko \
+    msm_drm.ko \
+    xiaomi_touch.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 
